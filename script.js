dd0f62b49db1b84c7fa90699a3f61f0164add33b
Chandana <165766482+chandanaamarendra@users.noreply.github.com>
2026-02-24 10:02:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,128 +1,45 @@
 const roles = [
 "Data Analyst",
 "Power BI Developer",
-"Advanced Excel Specialist",
 "SQL Developer",
-"Python Beginner",
+"Python Developer",
 "ML Enthusiast"
 ];
 
-let i=0;
-let j=0;
-let current="";
-let deleting=false;
+let i=0,j=0,current="",deleting=false;
 
 function type(){
 
 current=roles[i];
 
-if(deleting)
-document.getElementById("typing").textContent=current.substring(0,j--);
+document.getElementById("typing").textContent=current.substring(0,j);
 
-else
-document.getElementById("typing").textContent=current.substring(0,j++);
+if(!deleting && j<current.length){
 
-if(!deleting && j==current.length){
+j++;
 
-deleting=true;
-
-setTimeout(type,1000);
-
-return;
+setTimeout(type,100);
 
 }
 
-if(deleting && j==0){
+else if(deleting && j>0){
 
-deleting=false;
+j--;
 
-i=(i+1)%roles.length;
+setTimeout(type,50);
 
 }
 
-setTimeout(type,deleting?50:100);
+else{
+
+deleting=!deleting;
+
+if(!deleting)i=(i+1)%roles.length;
+
+setTimeout(type,800);
+
+}
 
 }
 
 type();
-
-particlesJS("particles-js", {
-
-particles: {
-
-number: {
-value: 100,
-density: {
-enable: true,
-value_area: 800
-}
-},
-
-color: {
-value: "#ff2d55"
-},
-
-shape: {
-type: "circle"
-},
-
-opacity: {
-value: 0.7,
-random: true
-},
-
-size: {
-value: 3,
-random: true
-},
-
-line_linked: {
-enable: false
-},
-
-move: {
-enable: true,
-speed: 2,
-direction: "none",
-random: true,
-straight: false,
-out_mode: "out"
-}
-
-},
-
-interactivity: {
-
-detect_on: "canvas",
-
-events: {
-
-onhover: {
-enable: true,
-mode: "repulse"
-},
-
-onclick: {
-enable: true,
-mode: "push"
-}
-
-},
-
-modes: {
-
-repulse: {
-distance: 120
-},
-
-push: {
-particles_nb: 4
-}
-
-}
-
-},
-
-retina_detect: true
-
-});
