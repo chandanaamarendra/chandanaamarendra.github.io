6c5129112090a4f7d48b2499de8e7129372a1357
Chandana <165766482+chandanaamarendra@users.noreply.github.com>
2026-02-24 10:41:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-/* TYPING */
-
-const roles=["Data Analyst","Power BI Developer","SQL Developer","Python Developer","ML Enthusiast"];
+const roles=[
+"Data Analyst",
+"Power BI Developer",
+"SQL Developer",
+"Python Developer",
+"ML Enthusiast"
+];
 
 let i=0,j=0,current="",deleting=false;
 
@@ -11,29 +15,19 @@ current=roles[i];
 document.getElementById("typing").textContent=current.substring(0,j);
 
 if(!deleting && j<current.length){
-
 j++;
-
 setTimeout(type,100);
-
 }
 
 else if(deleting && j>0){
-
 j--;
-
 setTimeout(type,50);
-
 }
 
 else{
-
 deleting=!deleting;
-
 if(!deleting)i=(i+1)%roles.length;
-
 setTimeout(type,800);
-
 }
 
 }
@@ -41,20 +35,13 @@ setTimeout(type,800);
 type();
 
 
-/* PARTICLES */
-
 particlesJS("particles-js",{
 
 particles:{
-
-number:{value:80},
-
+number:{value:60},
 color:{value:"#ff0000"},
-
 size:{value:3},
-
 move:{speed:2}
-
 }
 
 });
